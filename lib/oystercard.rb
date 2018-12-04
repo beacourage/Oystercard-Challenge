bc73f95375d26b467c336e99176eabebe36e0da5
beatricecourage <bea_courage@hotmail.co.uk>
2018-12-04 16:39:16 +0000

    raising error if maximum balance exceeded, deducting money

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,8 +2,24 @@ class Oystercard
 
   attr_reader :balance
 
+  MAXIMUM_BALANCE = 90
+
+
   def initialize
     @balance = 0
   end
 
+  def topup(amount)
+    maximum_balance = Oystercard::MAXIMUM_BALANCE
+    fail "Maximum balance of #{maximum_balance} exceeded" if amount + @balance > MAXIMUM_BALANCE
+    @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
 end
+
+# maximum_balance = Oystercard::MAXIMUM_BALANCE
+# Access a constant from outside the class
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,28 @@ describe Oystercard do
   it 'has a balance of zero' do
     expect(subject.balance).to eq(0)
   end
+
+  describe 'respinding to topup method' do
+    it { is_expected.to respond_to(:topup).with(1).argument }
+  end
+
+  it 'adding money to the card, topping up balance' do
+   expect{ subject.topup(1)}.to change{ subject.balance }.by(1)
+ end
+
+ it 'want maximum card limit, raises an error if the maximum balance is exceeded' do
+   maximum_balance = Oystercard::MAXIMUM_BALANCE
+   card = Oystercard.new
+   card.topup(maximum_balance)
+   expect{card.topup(1)}.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+ end
+
+
+  it { is_expected.to respond_to(:deduct).with(1).argument }
+
+  it 'fair deducted from my card' do
+    card = Oystercard.new
+    card.topup(20)
+    expect{ card.deduct(1)}.to change{ card.balance }.by (-1)
+  end
 end
